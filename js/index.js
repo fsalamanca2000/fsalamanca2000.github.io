bbe77b327ada7489e3a9b4afec023cf770f3ad87
Felipe Salamanca <juanfe689@gmail.com>
2019-12-13 15:55:00 -0500

    Media querys en JavaScript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+const ipad = window.matchMedia('screen and (max-width: 767px)')
 const menu = document.querySelector('.menu')
 const burgerButton = document.querySelector('#burger-menu')
 const linkP = document.querySelector('#linkPortfolio')
@@ -5,10 +6,22 @@ const linkE = document.querySelector('#linkEventos')
 const linkC = document.querySelector('#linkContacto')
 
 
-burgerButton.addEventListener('click', hideShow)
-linkP.addEventListener('click', linkHide)
-linkE.addEventListener('click', linkHide)
-linkC.addEventListener('click', linkHide)
+ipad.addListener(validation)
+
+function validation(event_size){
+  if(event.matches){
+    burgerButton.addEventListener('click', hideShow)
+    linkP.addEventListener('click', linkHide)
+    linkE.addEventListener('click', linkHide)
+    linkC.addEventListener('click', linkHide)
+  } else{
+    burgerButton.removeEventListener('click', hideShow)
+    linkP.removeEventListener('click', linkHide)
+    linkE.removeEventListener('click', linkHide)
+    linkC.removeEventListener('click', linkHide)
+  }
+  console.log(event_size)
+}
 
 function hideShow(){
   if(menu.classList.contains('is-active')){
